6ec7f6d8484cb3ab9c186dcfc3ae81a62a3b0d3e
mahmoudbassiony17 <bassiony12316@std.mans.edu.eg>
2023-06-23 09:29:49 +0300

    Finished Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import {FaBars} from 'react-icons/fa'
+import {AiOutlineClose} from 'react-icons/ai'
 const Navbar = () => {
+  const [open , setIsOpen] = useState(false);
+
   return (
-    <nav className="flex items-center justify-between w-full py-4 ">
-      <div className="m-5">
+    <nav className="flex flex-col items-center justify-between gap-2 w-full py-5 relative md:flex-row ">
+      <div className="w-full px-5 md:m-5 md:flex-[2]">
         <img src="/images/logo.png" alt="" className="w-[48px] aspect-square" />
       </div>
-      <ul className="flex items-center justify-center px-16 py-10 bg-white bg-opacity-[.04] backdrop-blur-[40.7px] gap-20 flex-1 max-w-[50%] tracking-[3px]">
-        <li className="text-16 font-Barlow text-white">
-          <Link to="/" className="before:content-['00'] before:mr-2">
+      {open ? <div className="md:hidden absolute top-[1.8rem] right-5" onClick={()=> setIsOpen((prev)=> !prev)}>
+         <AiOutlineClose className="text-secondary-500" size={25}/>
+      </div>:<div className="md:hidden absolute top-[1.8rem] right-5" onClick={()=> setIsOpen((prev)=> !prev)}>
+         <FaBars className="text-secondary-500" size={25}/>
+      </div>}
+      <ul className={`${open ? "flex" : "hidden"}  flex-col w-10/12 items-center justify-around px-16 bg-white bg-opacity-[.04] backdrop-blur-[40.7px] gap-8 flex-[3] tracking-[3px] md:flex md:flex-row`}>
+        <li className="text-16 font-Barlow text-white   relative hover:before:absolute hover:before:content-[''] hover:before:bottom-0 hover:before:h-1 hover:before:bg-secondary-500 hover:before:bg-opacity-[.5] hover:before:w-full">
+          <NavLink to="/" className="before:content-['00'] py-10 before:mr-2 ">
             Home
-          </Link>
+          </NavLink>
         </li>
-        <li className="text-16 font-Barlow text-white ">
-          <Link to="/destination" className="before:content-['01'] before:mr-2">
+        <li className="text-16 font-Barlow text-white relative hover:before:absolute hover:before:content-[''] hover:before:bottom-0 hover:before:h-1 hover:before:bg-secondary-500 hover:before:bg-opacity-[.5] hover:before:w-full">
+          <NavLink to="/destination" className="before:content-['01'] py-10 before:mr-2">
             Destination
-          </Link>
+          </NavLink>
         </li>
-        <li className="text-16 font-Barlow text-white ">
-          <Link to="/crew" className="before:content-['02'] before:mr-2">
+        <li className="text-16 font-Barlow text-white relative hover:before:absolute hover:before:content-[''] hover:before:bottom-0 hover:before:h-1 hover:before:bg-secondary-500 hover:before:bg-opacity-[.5] hover:before:w-full">
+          <NavLink to="/crew" className="before:content-['02'] py-10 before:mr-2">
             Crew
-          </Link>
+          </NavLink>
         </li>
-        <li className="text-16 font-Barlow text-white ">
-          <Link to="/tech" className="before:content-['03'] before:mr-2">
+        <li className="text-16 font-Barlow text-white relative hover:before:absolute hover:before:content-[''] hover:before:bottom-0 hover:before:h-1 hover:before:bg-secondary-500 hover:before:bg-opacity-[.5] hover:before:w-full">
+          <NavLink to="/tech" className="before:content-['03'] py-10 before:mr-2">
             Technology
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
